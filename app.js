81f918a515682de68517ad12846f8d4d87ef2ae7
memisemre <dev.emrememis@gmail.com>
2022-10-18 11:14:25 +0300

    Minor Changes For JS Interval Function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 setInterval(()=>{
     const newDate = new Date();
     document.querySelector(".hour-rod").style.transform = `rotate(${(newDate.getHours()*30)+180}deg)`;
-    document.querySelector(".minute-rod").style.transform = `rotate(${newDate.getMinutes()*6}deg)`;
-    document.querySelector(".second-rod").style.transform = `rotate(${newDate.getSeconds()*6}deg)`;
+    document.querySelector(".minute-rod").style.transform = `rotate(${(newDate.getMinutes()*6)+180}deg)`;
+    document.querySelector(".second-rod").style.transform = `rotate(${(newDate.getSeconds()*6)+180}deg)`;
 },1000);
